859b82af61a40d1ec6ab8a88e8dfe2a89c0f36fa
drzackyll <adams.z.d@gmail.com>
2017-04-07 11:32:47 -0400

    adds bootstrap, puts structure together

--- src/header.js
+++ src/header.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div style={{textAlign: "center"}}>
+      <h1>A l e x N u ñ e z</h1>
+      <h3>IT Consultant</h3>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">Home</div>
+          <div className="col-xs-2">About</div>
+          <div className="col-xs-2">FAQ</div>
+          <div className="col-xs-2">Hire Me</div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/home.js
+++ src/home.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Hello</div>
+    <div className="container-fluid">
+    <br />
+    <br />
+      <div className="row">
+        <div className="col-xs-3 col-xs-offset-1">
+          <img src={require("../public/images/martin.jpg")} style={{maxWidth: "100%", maxHeight: "100%"}} alt="Alex Nunez"/>
+        </div>
+        <div className="col-xs-3">
+          what i offer<br />
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </div>
+        <div className="col-xs-4">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."<br />
+          -Satisfied Customer 1<br /><br />
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."<br />
+          -Satisfied Customer 2
+        </div>
+      </div>
+    </div>
   )
 }
 
