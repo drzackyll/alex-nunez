70075d473fb6c2cc3e72c9e6848b3c12b455a060
drzackyll <adams.z.d@gmail.com>
2017-07-31 23:29:38 -0400

    Adds responsive navbar; needs work

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,8 @@ const About = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-sm-8 col-sm-offset-2">
+        <div className="col-sm-3 col-sm-offset-2" style={{borderStyle: "solid", height: "400px"}}>Headshot</div>
+        <div className="col-sm-5">
           I have worked in the information technology field for 20 years, but my interest in computers dates back over 30 years. I very much enjoy helping people solve whatever technology challenges they may be facing. I work with both home users and business owners to help them use technology to improve their lives & businesses.
         </div>
       </div>
--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,24 @@ import { Link } from 'react-router'
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+    <nav className="navbar navbar-inverse">
       <div className="container-fluid">
         <div className="navbar-header">
+          <button className="navbar-toggle navbar-toggle-right" data-toggle="collapse" data-target="#navbarToggler">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
           <a className="navbar-brand" href="/">A to Z</a>
         </div>
-        <p className="navbar-text">IT Consultants</p>
-        <ul className="nav navbar-nav">
-          <li><Link to="/smallbusiness">Small Business</Link></li>
-          <li><Link to="/homeuser">Home Users</Link></li>
-          <li><Link to="/testimonials">Testimonials</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarToggler">
+          <ul className="nav navbar-nav">
+            <li><Link to="/smallbusiness">Small Business</Link></li>
+            <li><Link to="/homeuser">Home Users</Link></li>
+            <li><Link to="/testimonials">Testimonials</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
