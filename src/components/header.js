ce51d77388ed0fa6176dee124e7d84b3996a5b45
drzackyll <adams.z.d@gmail.com>
2017-08-01 00:02:23 -0400

    Fixes navbar and responsiveness

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <nav className="navbar navbar-inverse">
       <div className="container-fluid">
         <div className="navbar-header">
-          <button className="navbar-toggle navbar-toggle-right" data-toggle="collapse" data-target="#navbarToggler">
+          <button className="navbar-toggle navbar-toggle-right" data-toggle="collapse" data-target="#navbarToggler" aria-expanded="false" aria-controls="navbar">
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
@@ -14,12 +14,12 @@ const Header = () => {
           <a className="navbar-brand" href="/">A to Z</a>
         </div>
         <div className="collapse navbar-collapse" id="navbarToggler">
-          <ul className="nav navbar-nav">
-            <li><Link to="/smallbusiness">Small Business</Link></li>
-            <li><Link to="/homeuser">Home Users</Link></li>
-            <li><Link to="/testimonials">Testimonials</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
+          <div className="nav navbar-nav" style={{textAlign: "center"}}>
+            <button className="btn-link navbar-btn"><Link to="/smallbusiness">Small Business</Link></button>
+            <button className="btn-link navbar-btn"><Link to="/homeuser">Home Users</Link></button>
+            <button className="btn-link navbar-btn"><Link to="/testimonials">Testimonials</Link></button>
+            <button className="btn-link navbar-btn"><Link to="/about">About</Link></button>
+          </div>
         </div>
       </div>
     </nav>
